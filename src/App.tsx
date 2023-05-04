2380635b022efd29cf431254a4d30f65daefb2c8
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2023-05-03 22:46:31 -0400

    Adição Todo Textfild + Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 
+import { AppBar, Button, CssBaseline, Grid, TextField, Toolbar, Typography, useTheme } from '@mui/material'
 import './App.css'
-import ThemeProvider from './ThemeProvider/index'
 
 function App() {
 
-
+  const theme = useTheme();
 
   return (
     <>
-    <ThemeProvider/>
-    <h1>asdas</h1>
+
+      <CssBaseline/>
+      <AppBar position="static">
+        <Toolbar sx={{
+          paddingTop: theme.spacing(2),
+          paddingBottom: theme.spacing(2),
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          '@media all': {
+            minHeight: 200,
+          }
+        }}>
+          <Typography variant='h5' component='h1'>TODO</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Grid container spacing={theme.spacing(0.5)}>
+          <Grid item xl={10} sm={12}>
+            <TextField name="task" fullWidth />
+          </Grid>
+          <Grid item xl={2} sm={12}>
+            <Button variant="contained" fullWidth sx={{"height":"100%"}}>Criar</Button>
+          </Grid>
+        </Grid>
+      </main>
+
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
